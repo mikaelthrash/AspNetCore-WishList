32c63b05224e110eada95aaf5d153261d6755356
Mr. Thrash <mikael.thrash@ruralsource.com>
2018-06-25 09:12:35 -0500

    ApplicationDbContext : DbContext

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext 
+{
+	public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+	}
+}
